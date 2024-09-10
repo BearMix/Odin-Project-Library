dc829b6d79aaab05a7cf3c6fc9a55963c4b93dbc
BearMix <Mix8704@gmail.com>
2024-09-10 09:11:09 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,53 +1,61 @@
-let library = [];
-
-function Book(title, author, pages, isRead) {
-  this.title = title;
-  this.author = author;
-  this.pages = pages;
-  this.isRead = isRead;
+class Book {
+  constructor(title, author, pages, isRead) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.isRead = isRead;
+  }
 }
 
-function addBookToLibrary(book) {
-  library.push(book);
-  displayBooks();
+class Library {
+  constructor() {
+    this.library = [];
+  }
+
+  addBook(book) {
+    this.library.push(book);
+    this.displayBooks();
+  }
+
+  displayBooks() {
+    const bookList = document.getElementById("book-list");
+    bookList.innerHTML = "";
+
+    this.library.forEach((book, index) => {
+      const bookCard = document.createElement("div");
+      bookCard.classList.add("book-card");
+
+      const buttonClass = book.isRead ? "finished-btn" : "read-btn";
+
+      bookCard.innerHTML = `
+                <h3>${book.title}</h3>
+                <p><span>By:</span> ${book.author}</p>
+                <p><span>Pages:</span> ${book.pages}</p>
+                <p><span>Read Yet?</span> ${book.isRead ? "Yes" : "No"}</p>
+                <div class="card-btn">
+                  <button class="${buttonClass}" onclick="library.toggleReadStatus(${index})">
+                      ${book.isRead ? "I've Finished!" : "Not Finished"}
+                  </button>
+                  <button class="remove-btn" onclick="library.removeBook(${index})">Delete</button>
+                </div>
+            `;
+
+      bookList.appendChild(bookCard);
+    });
+  }
+
+  removeBook(index) {
+    this.library.splice(index, 1);
+    this.displayBooks();
+  }
+
+  toggleReadStatus(index) {
+    this.library[index].isRead = !this.library[index].isRead;
+    this.displayBooks();
+  }
 }
 
-function displayBooks() {
-  const bookList = document.getElementById("book-list");
-  bookList.innerHTML = "";
-
-  library.forEach((book, index) => {
-    const bookCard = document.createElement("div");
-    bookCard.classList.add("book-card");
-
-    const buttonClass = book.isRead ? "finished-btn" : "read-btn";
-
-    bookCard.innerHTML = `
-            <h3>${book.title}</h3>
-            <p><span>By:</span> ${book.author}</p>
-            <p><span>Pages:</span> ${book.pages}</p>
-            <p><span>Read Yet?</span> ${book.isRead ? "Yes" : "No"}</p>
-            <div class="card-btn">
-              <button class="${buttonClass}" onclick="toggleReadStatus(${index})">
-                  ${book.isRead ? "I've Finished!" : "Not Finished"}
-              </button>
-              <button class="remove-btn" onclick="removeBook(${index})">Delete</button>
-            </div>
-        `;
-
-    bookList.appendChild(bookCard);
-  });
-}
-
-function removeBook(index) {
-  library.splice(index, 1);
-  displayBooks();
-}
-
-function toggleReadStatus(index) {
-  library[index].isRead = !library[index].isRead;
-  displayBooks();
-}
+const library = new Library();
 
 document.getElementById("new-book-btn").addEventListener("click", () => {
   document.getElementById("new-book-form").classList.remove("hidden");
@@ -69,20 +77,20 @@ document.getElementById("new-book-form").addEventListener("submit", (event) => {
   const isRead = document.getElementById("read").checked;
 
   const newBook = new Book(title, author, pages, isRead);
-  addBookToLibrary(newBook);
+  library.addBook(newBook);
 
   document.getElementById("new-book-form").classList.add("hidden");
   document.getElementById("form-container").classList.remove("form-background");
   document.getElementById("new-book-form").reset();
 });
 
-addBookToLibrary(new Book("Game of Thrones", "George R. R. Martin", 694, true));
-addBookToLibrary(new Book("1984", " George Orwell", 328, true));
-addBookToLibrary(new Book("Harry Potter", "J.K. Rowling", 500, false));
-addBookToLibrary(
+library.addBook(new Book("Game of Thrones", "George R. R. Martin", 694, true));
+library.addBook(new Book("1984", "George Orwell", 328, true));
+library.addBook(new Book("Harry Potter", "J.K. Rowling", 500, false));
+library.addBook(
   new Book("The Lord of the Rings", "J.R.R. Tolkien", 1216, true)
 );
-addBookToLibrary(new Book("The Hobbit", "J.R.R. Tolkien", 310, false));
-addBookToLibrary(
+library.addBook(new Book("The Hobbit", "J.R.R. Tolkien", 310, false));
+library.addBook(
   new Book("The Great Gatsby", "F. Scott Fitzgerald", 180, false)
 );
